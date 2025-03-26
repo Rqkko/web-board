39268ce323bdc3e364f57ddb968508ac3143a6fe
Rqkko <rakko26@gmail.com>
2025-03-26 10:41:21 +0700

    Create config.ts

---
+++ server/src/config.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+    supabaseUrl: process.env.SUPABASE_URL || "",
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
+    port: process.env.PORT || 3001
+};
