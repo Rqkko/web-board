2fedb72d477f88200d83e509621825d5018dc86a
Rqkko <rakko26@gmail.com>
2025-03-26 21:14:53 +0700

    Setup Swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 import { config } from "./config";
 import userRoutes from './routes/userRoutes';
 import postRoutes from './routes/postRoutes';
+import { setupSwagger } from "./swagger";
 
 const app = express();
 app.use(cors());
@@ -12,12 +13,15 @@ app.use(express.json());
 const supabase = createClient(config.supabaseUrl, config.supabaseAnonKey);
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+
+// Setup Swagger
+setupSwagger(app);
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
     if (config.nodeEnv === "development") {
-        console.log(`For Dev: http://localhost:${config.port}`);
+        console.log(`For Dev: http://localhost:${config.port}/api-docs`);
     }
 });
