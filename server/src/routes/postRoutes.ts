109e9c3a980044b79ad8a8e71a9a9cd8b41c6c83
Rqkko <rakko26@gmail.com>
2025-03-26 21:15:32 +0700

    Add Test API docs

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -2,6 +2,15 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/post/getPosts:
+ *   get:
+ *     summary: Get all posts
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved posts
+ */
 router.get('/getPosts', (req, res) => {
   res.json({ message: 'Just pretend this is a post' });
 });
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -2,10 +2,37 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/user/getName:
+ *   get:
+ *     summary: Get the name of user
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved users
+ */
 router.get('/getName', (req, res) => {
   res.json({ message: "Rakko"});
 });
 
+/**
+ * @swagger
+ * /api/user/createAccount:
+ *   post:
+ *     summary: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
 // router.post('/createAccount', (req, res) => {
 //   // TODO
 // });
