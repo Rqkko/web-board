a2e9259b948bba16e242d265c04fff9fa858e3b1
Rqkko <rakko26@gmail.com>
2025-03-27 22:51:41 +0700

    Modify to serve frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import { config } from "./config";
 import userRoutes from './routes/userRoutes';
 import postRoutes from './routes/postRoutes';
 import { setupSwagger } from "./swagger";
+import path from "path";
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,14 @@ app.use('/api/post', postRoutes);
 
 // Setup Swagger
 setupSwagger(app);
+
+// Serve React frontend for non-API routes
+const clientBuildPath = path.join(__dirname, '../../client/build');
+app.use(express.static(clientBuildPath)); // Serve static files from React build directory
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(clientBuildPath, 'index.html')); // Serve React's index.html for all other routes
+});
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
