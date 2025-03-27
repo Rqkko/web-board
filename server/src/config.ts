f63e4c5cf39b13a368097a23d712e9fbe734e91b
Rqkko <rakko26@gmail.com>
2025-03-27 13:32:18 +0700

    Refactor port config
    - Include ':' only if the PORT exists

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,12 +1,11 @@
 import dotenv from "dotenv";
-import os from 'os';
 
 dotenv.config();
 
 export const config = {
     supabaseUrl: process.env.SUPABASE_URL || "",
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
-    port: process.env.PORT || "",
+    port: process.env.PORT ? `:${process.env.PORT}` : "",
     nodeEnv: process.env.NODE_ENV || "",
     // hostName: process.env.HOST_NAME || os.hostname(),
     hostName: process.env.RAILWAY_PUBLIC_DOMAIN || process.env.HOSTNAME || 'localhost',
