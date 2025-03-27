077fe37fb406fc6705c1fc14c6ad223a78d02d04
Rqkko <rakko26@gmail.com>
2025-03-27 13:23:16 +0700

    Refactor URL configs
    - No port when on Railway

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,10 +6,11 @@ dotenv.config();
 export const config = {
     supabaseUrl: process.env.SUPABASE_URL || "",
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || "",
     nodeEnv: process.env.NODE_ENV || "",
     // hostName: process.env.HOST_NAME || os.hostname(),
     hostName: process.env.RAILWAY_PUBLIC_DOMAIN || process.env.HOSTNAME || 'localhost',
     // hostName: process.env.NODE_ENV === 'development' ? 'localhost' : os.hostname(),
     protocol: process.env.NODE_ENV === 'development' ? 'http' : 'https',
+    
 };
