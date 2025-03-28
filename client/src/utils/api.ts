5b9be66bfa9bcf373735f79c94c94d47333a3803
Rqkko <rakko26@gmail.com>
2025-03-28 15:44:55 +0700

    Use relative path for production frontend

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+// Check if the app is running on localhost
+const isDev = window.location.hostname === 'localhost';
+const API_BASE_URL = isDev
+  ? 'http://localhost:3001' // Development backend URL
+  : '/'; // Production backend use the same path as frontend
+
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
