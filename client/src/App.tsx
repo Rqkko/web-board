fe7897972d73d681f5eb8b0b67226eb517e32694
Rqkko <rakko26@gmail.com>
2025-03-29 11:55:15 +0700

    Create "Post" page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Post from "pages/Post";
+
+function PostWrapper() {
+  const { postId } = useParams<{ postId: string }>(); // Extract postId from the URL
+
+  return <Post postId={postId} />;
+}
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/post/:postId" element={<PostWrapper />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
