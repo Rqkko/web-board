5c349d8dd0a70419b4946e022eb7d3678ebce1be
Rqkko <rakko26@gmail.com>
2025-04-20 23:24:03 +0700

    Refactor: Remove Router Import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Router, Routes, Route, useParams } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Post from "pages/Post";
