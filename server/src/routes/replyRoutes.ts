d1d7ebb34392e457de26f8d13546b038828fea4d
Rqkko <rakko26@gmail.com>
2025-04-27 14:07:49 +0700

    Update createReply

--- server/src/routes/replyRoutes.ts
+++ server/src/routes/replyRoutes.ts
@@ -1,10 +1,37 @@
 import { Router } from 'express';
-import { getReplies } from '../controllers/replyController';
+import { createReply, getReplies } from '../controllers/replyController';
 import { asyncHandler } from '../utils/asyncHandler';
 
 const router = Router();
 
-// router.post('/:postId', asyncHandler(createReply));
+/**
+ * @swagger
+ * /api/reply/{postId}:
+ *   post:
+ *     summary: Create a reply to a post
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         required: true
+ *         description: ID of the post to reply to
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Reply created successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post('/:postId', asyncHandler(createReply));
 router.get('/:postId', asyncHandler(getReplies));
 
 export default router;
