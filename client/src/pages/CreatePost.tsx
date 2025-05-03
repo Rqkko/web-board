19d65bd35e2487882d7150e8388556a10d3e648f
Rqkko <rakko26@gmail.com>
2025-05-03 17:29:59 +0700

    feat: create 'title' & 'content'

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+import PostTextField from 'components/PostTextField'
+import { useState } from 'react'
 
 function CreatePost() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
-    <div style={{ marginTop: 30}}>CreatePost</div>
+    <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: '#FFFFFF',
+      paddingTop: '100px',
+    }}
+    >
+      <Typography style={{ color: 'black', fontSize: '50px', fontWeight: 'bold', marginBottom: '20px' }}>What's on your mind?</Typography>
+      
+      <Typography style={{ color: 'black', fontSize: '24px', fontWeight: 'bold', alignSelf: 'start', marginLeft: '25%', marginTop: '20px' }}>Title</Typography>
+      <PostTextField
+        type="text"
+        value={title}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setTitle(e.target.value)}}
+      />
+
+      <Typography style={{ color: 'black', fontSize: '24px', fontWeight: 'bold', alignSelf: 'start', marginLeft: '25%', marginTop: '50px' }}>Description</Typography>
+      <PostTextField
+        type="text"
+        value={content}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setContent(e.target.value)}}
+        multiline={true}
+      />
+    </div>
   )
 }
 
