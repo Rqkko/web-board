f71dac4da9b4a56b5be951c005763feea3a65a7c
Rqkko <rakko26@gmail.com>
2025-05-03 18:36:04 +0700

    feat: add RoomPicker

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,10 +1,12 @@
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import PostTextField from 'components/PostTextField'
+import RoomPicker from 'components/RoomPicker';
 import { useState } from 'react'
 
 function CreatePost() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [room, setRoom] = useState<number | null>(null);
 
   return (
     <div
@@ -17,7 +19,7 @@ function CreatePost() {
       paddingTop: '100px',
     }}
     >
-      <Typography style={{ color: 'black', fontSize: '50px', fontWeight: 'bold', marginBottom: '20px' }}>What's on your mind?</Typography>
+      <Typography style={{ color: 'black', fontSize: '50px', fontWeight: 'bold', marginBottom: '15px' }}>What's on your mind?</Typography>
       
       <Typography style={{ color: 'black', fontSize: '24px', fontWeight: 'bold', alignSelf: 'start', marginLeft: '25%', marginTop: '20px' }}>Title</Typography>
       <PostTextField
@@ -26,13 +28,37 @@ function CreatePost() {
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setTitle(e.target.value)}}
       />
 
-      <Typography style={{ color: 'black', fontSize: '24px', fontWeight: 'bold', alignSelf: 'start', marginLeft: '25%', marginTop: '50px' }}>Description</Typography>
+      <Typography style={{ color: 'black', fontSize: '24px', fontWeight: 'bold', alignSelf: 'start', marginLeft: '25%', marginTop: '40px' }}>Description</Typography>
       <PostTextField
         type="text"
         value={content}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setContent(e.target.value)}}
         multiline={true}
       />
+
+      <Typography style={{ color: 'black', fontSize: '24px', fontWeight: 'bold', alignSelf: 'start', marginLeft: '25%', marginTop: '40px', marginBottom: '20px' }}>Room</Typography>
+      <RoomPicker 
+        selectedRoom={room}
+        setSelectedRoom={setRoom}
+      />
+
+      <Button
+        variant="contained"
+        style={{
+          backgroundColor: '#F08A5D',
+          color: 'white',
+          fontSize: '24px',
+          fontWeight: 'bold',
+          marginTop: '50px',
+          padding: '10px 20px',
+        }}
+        onClick={() => {
+          // TODO Handle post creation logic here
+          console.log('Post created with title:', title, 'and content:', content, 'in room:', room);
+        }}
+      >
+        Create Post
+      </Button>
     </div>
   )
 }
