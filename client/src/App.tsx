4e3b978d37eb61bd470094714d7d40bdce3c7f3b
Rqkko <rakko26@gmail.com>
2025-05-05 20:49:38 +0700

    style: remove comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,13 +8,6 @@ import CustomAppBar from "components/CustomAppBar";
 import Logout from "./pages/Logout";
 import Home from './pages/Home';
 import PostDetail from './pages/PostDetail';
-// import Prioritized from './pages/Prioritized'; // Make sure this matches the filename casing
-
-// function PostWrapper() {
-//   const { postId } = useParams<{ postId: string }>(); // Extract postId from the URL
-
-//   return <Post postId={postId} />;
-// }
 
 function App() {
   const location = useLocation();
@@ -28,12 +21,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/logout" element={<Logout />} />
-        {/* <Route path="/post/:postId" element={<PostWrapper />} /> */}
-        {/* <Route path="/" element={<Prioritized />} /> Prioritized route */}
+        <Route path="/posts/:id" element={<PostDetail />} /> 
         <Route path="/create-post" element={<CreatePost />} />
         <Route path="*" element={<NotFound />} />
-        {/* <Route path="/post/:id" element={<Post />} /> */}
-        <Route path="/posts/:id" element={<PostDetail />} /> 
       </Routes>
     </>
   );
