ca88c98c6cc5c98671d9605acca2241eb651fd2e
Rqkko <rakko26@gmail.com>
2025-05-05 22:04:06 +0700

    feat: add room name on card

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/Post.module.css';
+import { api } from 'utils/api';
 
 type PostProps = {
   id: string;
@@ -13,16 +14,28 @@ type PostProps = {
 
 const Post: React.FC<PostProps> = ({ id, username, profilePic, title, description, image }) => {
   const navigate = useNavigate();
+  const [room, setRoom] = useState('');
 
   const handleClick = () => {
     navigate(`/posts/${id}`);
   };
+
+  useEffect(() => {
+    api.get(`/api/room/${id}`)
+      .then((response) => {
+        setRoom(response.data.data.title);
+      })
+      .catch((error) => {
+        console.error('Error fetching room data:', error);
+      });
+  })
 
   return (
     <div className={styles.postContainer} onClick={handleClick}>
       <div className={styles.header}>
         <img src={profilePic} alt={`${username}'s profile`} className={styles.profilePic} />
         <span className={styles.username}>{username}</span>
+        <div className={styles.room}>{room}</div>
       </div>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
