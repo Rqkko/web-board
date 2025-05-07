a689fb2a3705c3b7f97cd186637b53518e99440e
Rqkko <rakko26@gmail.com>
2025-05-07 22:30:38 +0700

    feat: return username for '/api/reply/{id}'

--- server/src/controllers/replyController.ts
+++ server/src/controllers/replyController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import supabase from '../supabaseClient';
+import { getUsername } from '../utils/usernameGetter';
 
 export const createReply = async (req: Request, res: Response): Promise<void> => {
   const { content } = req.body;
@@ -18,13 +19,35 @@ export const createReply = async (req: Request, res: Response): Promise<void> =>
 export const getReplies = async (req: Request, res: Response): Promise<void> => {
   const postId = req.params.postId;
 
-  const { data, error } = await supabase
+  const { data: replies, error } = await supabase
     .from('replies')
     .select('*')
     .eq('post_id', postId)
     .order('created_at', { ascending: false});
 
-  if (error) {res.status(400).json({ error: error.message })}
+  if (error) {
+    res.status(400).json({ error: error.message })
+    return;
+  }
 
-  res.status(200).json({ data });
+  try {
+    const decoratedPosts = await Promise.all(
+      replies.map(async (reply) => {
+        let username;
+
+        try {
+          username = await getUsername(reply.user_id) || 'Unknown User';
+        } catch (err) {
+          console.error('Error fetching username:', err);
+        }
+        console.log('username:', username);
+        return { ...reply, username };
+      })
+    );
+
+    res.status(200).json({ data: decoratedPosts });
+  } catch (err) {
+    console.error('Error decorating posts:', err);
+    res.status(500).json({ error: 'Failed to fetch posts' });
+  }
 };
