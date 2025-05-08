f3e6385c172687f0e6d0e745dbc861c3d4e81441
Rqkko <rakko26@gmail.com>
2025-05-08 22:42:45 +0700

    perf: remove unused import

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/Post.module.css';
 import { api } from 'utils/api';
-import { Button, IconButton } from '@mui/material';
+import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 type PostProps = {
